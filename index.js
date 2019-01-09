b39557a20afb9822831edfaa0a9fdeebf23b3916
Dongwoo Lee <likedemian@me.com>
2019-01-09 19:55:03 +0900

    Added each map filter

--- index.js
+++ index.js
@@ -16,16 +16,12 @@ var tempUsers = []
 for (var i = 0; i < users.length; i++) {
   if (users[i].age >= 30) tempUsers.push(users[i])
 }
-console.log(tempUsers)
-
 // 2. 30세 이상인 users의 name을 수집.
 
 var names = []
 for (var i = 0; i < tempUsers.length; i++) {
   names.push(tempUsers[i].name)
 }
-console.log(names)
-
 
 // 3. 30세 미만인 users를 거른다.
 var tempUsers = []
@@ -34,72 +30,58 @@ for (var i = 0; i < users.length; i++) {
     tempUsers.push(users[i])
   }
 }
-console.log(tempUsers)
-
 
 // 4. 30세 미만인 users의 ages를 수집한다.
 var ages = []
 for (var i = 0; i < tempUsers.length; i++) {
   ages.push(tempUsers[i].age)
 }
-console.log(ages)
 
 
-
-// 2. _filter, _map으로 리팩토링.
+// 2. _filter, _map으로 리팩토링
 
 function _filter(users = [], predicate) {
   var newList = []
-  for (var i = 0; i < users.length; i++) {
-    if (predicate(users[i])) {
-      newList.push(users[i])
-    }
-  }
+  _each(users, val => {
+    if (predicate(val)) newList.push(val)
+  })
   return newList
 }
-
 
 
 function _map(list = [], mapper) {
   var newList = []
-  for (var i = 0; i < list.length; i++) {
-    newList.push(mapper(list[i]))
-  }
+  _each(list, val => {
+    newList.push(mapper(val))
+  })
   return newList
 }
 
 
-var over30 = _filter(users, function(user) { return user.age >= 30 })
-var under30 = _filter(users, function(user) { return user.age < 30 })
+function _each(list, iter) {
+  for (var i = 0; i < list.length; i++) { iter(list[i]) }
+  return list
+}
+
+
+console.log(Array.from(document.querySelectorAll('*')))
+console.log(_map(document.querySelectorAll('*'), node => node.nodeName))
+
+
+var over30 = _filter(users, user => user.age >= 30)
+var under30 = _filter(users, user => user.age < 30)
+var names = _map(over30, user => user.name)
+var ages = _map(under30, user => user.age)
 
 
 console.log(
-  _filter(users, function(user) { return user.age >= 30 }),
-  _filter(users, function(user) { return user.age < 30 }),
-  _filter([1, 2, 3, 4], function(num) { return num % 2 }),
-  _filter([1, 2, 3, 4], function(num) { return !(num % 2) })
+  _filter(users, user => user.age >= 30),
+  _filter(users, user => user.age < 30),
+  _filter([1, 2, 3, 4], num => num % 2),
+  _filter([1, 2, 3, 4], num => !(num % 2))
 )
 console.log(over30)
-
-var names = _map(over30, function(user) { return user.name })
 console.log(names)
-
-var ages = _map(under30, function(user) { return user.age })
 console.log(ages)
-
-
-
-
-console.log(
-  _map(
-    _filter(users, function(user) { return user.age >= 30 }),
-    function(user) { return user.name }
-  )
-)
-
-console.log(
-  _map(
-    _filter(users, function(user) { return user.age < 30 }),
-    function(user) { return user.age }
-  )
-)
+console.log(_map(_filter(users, user => user.age >= 30), user => user.name))
+console.log(_map(_filter(users, user => user.age < 30), user => user.age))
